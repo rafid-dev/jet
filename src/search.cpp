bb19faf9f632b35e9259a873354d6fad37a11eac
rafid-dev <vorahsan@gmail.com>
2023-12-29 14:16:25 +0600

    Bench: 15910036

--- src/search.cpp
+++ src/search.cpp
@@ -204,7 +204,7 @@ namespace jet {
 
                 bool do_fullsearch = !isPvNode || movecount > 1;
 
-                if (!inCheck && isQuiet && movecount > 4 && depth >= 3) {
+                if (!inCheck && isQuiet && movecount > 2 && depth >= 3) {
                     Depth reduction = LmrTable[std::min(depth, 63)][std::min(movecount, 63)];
 
                     score = -negamax<NodeType::NONPV>(-alpha - 1, -alpha, depth - reduction, st, ss + 1);
