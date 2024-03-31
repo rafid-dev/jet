cd4727d1fbd9f96d277200a9471753abaf0eeb5f
rafid-dev <vorahsan@gmail.com>
2024-03-31 14:58:13 +0600

    Bench: 2350856

--- src/nnue/constants.hpp
+++ src/nnue/constants.hpp
@@ -5,9 +5,9 @@ namespace jet {
 
         namespace constants {
 
-            constexpr int BUCKETS     = 16;
+            constexpr int BUCKETS     = 32;
             constexpr int INPUT_SIZE  = 64 * 6 * 2 * BUCKETS;
-            constexpr int HIDDEN_SIZE = 512;
+            constexpr int HIDDEN_SIZE = 384;
             constexpr int OUTPUT_SIZE = 1;
 
             constexpr int INPUT_LAYER_SIZE  = INPUT_SIZE * HIDDEN_SIZE;
@@ -22,11 +22,11 @@ namespace jet {
                 0,  1,  2,  3,  3,  2,  1,  0,
                 4,  5,  6,  7,  7,  6,  5,  4,
                 8,  9,  10, 11, 11, 10, 9,  8,
-                8,  9,  10, 11, 11, 10, 9,  8,
-                12, 12, 13, 13, 13, 13, 12, 12,
-                12, 12, 13, 13, 13, 13, 12, 12,
-                14, 14, 15, 15, 15, 15, 14, 14,
-                14, 14, 15, 15, 15, 15, 14, 14,
+                12, 13, 14, 15, 15, 14, 13, 12,
+                16, 17, 18, 19, 19, 18, 17, 16,
+                20, 21, 22, 23, 23, 22, 21, 20,
+                24, 25, 26, 27, 27, 26, 25, 24,
+                28, 29, 30, 31, 31, 30, 29, 28,
             };
             // clang-format on
 
